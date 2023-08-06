78ab470544e3fe2d54234b4569a07e5ae3f32822
Yo Joe <yo.joe@example.com>
2023-08-06 17:00:58 -0500

    styling

---
+++ app/foobarey/page.jsx
@@ -0,0 +1,5 @@
+import stuff from "./page.module.css";
+
+export default function Page() {
+  return <div class={stuff.uge}>Hiya</div>;
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
     <section className="py-24">
       <div className="container">
-        <h1 className="mt-4 text-3xl font-bold">NextJs 13 Starter Kit</h1>
+        <h1 className={styles.title}>NextJs 13 Starter Kit</h1>
+        {/* mt-4 text-3xl font-bold */}
       </div>
     </section>
   );
